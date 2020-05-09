4daa363e98a4d5672821527bbb140d3b8ef76447
dominc8 <kormandominik@gmail.com>
2020-05-09 17:46:41 +0200

    Added temperature info

--- blocks.h
+++ blocks.h
@@ -3,17 +3,19 @@ static const Block blocks[] = {
     /*Icon*/    /*Command*/        /*Update Interval*/    /*Update Signal*/
 //    {"", "cat ~/.pacupdate | sed /📦0/d",                                 0,        9},
 
-      {"🧠", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",    30,        0},
+      {"🧠", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",                                 20,         0},
 
-      {"", "~/.config/statusbar/volume",                                    1,        0},
+      {"🌡", "sensors | awk '/^Core 0:/{printf \"%d\", $3} ; /^Core 1:/{printf \"/%d°C\\n\", $3};' ",    15,         0},
+//🌡
+      {"", "~/.config/statusbar/volume",                                                                1,          0},
 
 //    {"☀", "xbacklight | sed 's/\\..*//'",                                 0,        11},
 
-      {"", "~/.config/statusbar/battery",                                   5,        0},
+      {"", "~/.config/statusbar/battery",                                                               5,          0},
 
-//    {"🌡", "sensors | awk '/^temp1:/{print $2}'"                           5,        0},
+      {"", "~/.config/statusbar/internet",                                                              10,         0},
 
-      {"", "date +%H:%M",                                                   5,        0},
+      {"", "date +'%H:%M %d/%b/%Y'",                                                                    5,          0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
